69cd06e1550a2499514fb2a9ce208bfc4af71858
Ressl Robert <robert.ressl@avectris.ch>
2018-07-03 13:58:03 +0200

    :pencil: add supports os

--- metadata.rb
+++ metadata.rb
@@ -7,14 +7,9 @@ long_description 'Installs/Configures sudo_databag'
 version '0.1.0'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-issues_url 'https://git.zmssrv.com/avectris/chef/chef_sudo_databag/issues'
+%w(redhat centos).each do |os|
+  supports os
+end
 
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
+issues_url 'https://git.zmssrv.com/avectris/chef/chef_sudo_databag/issues'
 source_url 'https://git.zmssrv.com/avectris/chef/chef_sudo_databag'
