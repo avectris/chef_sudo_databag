de26a06562148548557aecb09a89020639388d9d
Ressl Robert <robert.ressl@avectris.ch>
2018-10-29 18:29:34 +0100

    :umbrella: add example for defaults user config
    Issue #10

--- test/integration/default/config_test.rb
+++ test/integration/default/config_test.rb
@@ -7,6 +7,10 @@ describe file('/etc/sudoers') do
   its('content') { should match(/Defaults env_reset/) }
   its('content') { should match(%r{Defaults secure_path = "\/sbin:\/bin:\/usr\/sbin:\/usr\/bin"}) }
   its('content') { should match(/Defaults env_keep = "COLORS DISPLAY HOSTNAME HISTSIZE KDEDIR LS_COLORS MAIL PS1 PS2 QTDIR USERNAME LANG LC_ADDRESS LC_CTYPE LC_COLLATE LC_IDENTIFICATION LC_MEASUREMENT LC_MESSAGES LC_MONETARY LC_NAME LC_NUMERIC LC_PAPER LC_TELEPHONE LC_TIME LC_ALL LANGUAGE LINGUAS _XKB_CHARSET XAUTHORITY"/) }
+  its('content') { should match(/Defaults:ressl_robert !requiretty/) }
+  its('content') { should match(/Defaults:ressl_robert !env_reset/) }
+  its('content') { should match(/Defaults:robert_ressl !requiretty/) }
+  its('content') { should match(/Defaults:robert_ressl !env_reset/) }
 end
 
 describe file('/etc/sudoers') do
